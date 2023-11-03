1af1efda8df0299ba7bf09b22a7e2ed23d741fb7
Bohdan Hulobov <bogdan.gulobov@gmail.com>
2023-11-03 16:34:17 +0200

    lesson_8 homework

---
+++ modules/dev.js
@@ -0,0 +1,11 @@
+import { Employee } from "./employee.js";
+
+export class Dev extends Employee {
+  getJobType() {
+    return "I am a Dev";
+  }
+
+  static returnArrayOfDevs(...devs) {
+    return devs.filter((dev) => dev instanceof Dev);
+  }
+}
